bd06789eeee6fd787846d161bb95348837691bc3
Valentina <correavalentinalaura@gmail.com>
2021-06-01 16:00:53 -0300

    agrego pokemones al sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,16 @@
 /* Tabla de pokemones */
-INSERT INTO pokemones (id_pokemon,nombre_pokemon,nivel_de_arribo) VALUES (0,'Pikachu',45);
-INSERT INTO pokemones (id_pokemon,nombre_pokemon,nivel_de_arribo) VALUES (1,'Charmander',50);
-INSERT INTO pokemones (id_pokemon,nombre_pokemon,nivel_de_arribo) VALUES (2,'Bolbasour',40);
-INSERT INTO pokemones (id_pokemon,nombre_pokemon,nivel_de_arribo) VALUES (3,'Ratata',30);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (0,'Pikachu',1);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (1,'Charmander',2);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (2,'Bolbasour',3);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (3,'Ratata',4);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (4,'Rauchu',21);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (5,'Charmeleon',22);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (6,'Charizard',32);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (7,'Ivasaur',23);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (8,'Venusaur',33);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (9,'Ratacate',24);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (10,'Pidgey',5);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (11,'Pidgeotto',15);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (12,'Pidgeot',25);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (13,'Meowth',6);
+INSERT INTO pokemones (id,nombre,nivel) VALUES (14,'Persian',16);
