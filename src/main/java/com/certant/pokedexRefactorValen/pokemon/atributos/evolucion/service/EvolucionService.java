87f47b1aee233edb0fc944d582f18d5800ddd584
Valentina <correavalentinalaura@gmail.com>
2021-06-02 11:59:38 -0300

    add repo y service (sigue imprimiento las otras)

--- src/main/java/com/certant/pokedexRefactorValen/pokemon/atributos/evolucion/service/EvolucionService.java
+++ src/main/java/com/certant/pokedexRefactorValen/pokemon/atributos/evolucion/service/EvolucionService.java
@@ -1,4 +1,18 @@
 package com.certant.pokedexRefactorValen.pokemon.atributos.evolucion.service;
 
-public class EvolucionService {
+import com.certant.pokedexRefactorValen.pokemon.atributos.evolucion.entity.Evolucion;
+import com.certant.pokedexRefactorValen.pokemon.atributos.evolucion.repository.IEvolucionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EvolucionService implements IEvolucionService{
+    @Autowired
+    IEvolucionRepository evolucionRepository;
+
+    @Override
+    public List<Evolucion> findAll() {
+        return (List<Evolucion>) evolucionRepository.findAll();
+    }
+
 }
--- src/main/java/com/certant/pokedexRefactorValen/pokemon/atributos/evolucion/service/IEvolucionService.java
+++ src/main/java/com/certant/pokedexRefactorValen/pokemon/atributos/evolucion/service/IEvolucionService.java
@@ -1,4 +1,10 @@
 package com.certant.pokedexRefactorValen.pokemon.atributos.evolucion.service;
 
+import com.certant.pokedexRefactorValen.pokemon.atributos.evolucion.entity.Evolucion;
+import java.util.List;
+
 public interface IEvolucionService {
+
+    List<Evolucion> findAll();
+
 }
