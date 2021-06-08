78e2783171baa4a76bef596230ec8b54981ffba2
Valentina <correavalentinalaura@gmail.com>
2021-06-08 11:38:11 -0300

    add seguridad y alerta de bienvenida

---
+++ src/main/resources/templates/codigo.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    //alert("hola");
+    /*$("button").click(function() {
+        $("#botoningreso").on({
+        mouseleave: function(){
+            $(this).css("background-color", "red");
+        },
+        click: function(){
+            $(this).css("background-color", "red");
+        }
+    });
+    });*/
+});
