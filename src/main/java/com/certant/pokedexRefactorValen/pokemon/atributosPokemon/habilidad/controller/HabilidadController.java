a9df1f25ac7387a42fd131beba0988e6337e5b1d
Valentina <correavalentinalaura@gmail.com>
2021-06-11 18:51:24 -0300

    cambio la forma de la request

--- src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/habilidad/controller/HabilidadController.java
+++ src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/habilidad/controller/HabilidadController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.http.HttpRequest;
 import java.util.List;
 
 @Controller
