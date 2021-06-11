340e16e805e3296fee8ce0a4892f066373079d7e
Valentina <correavalentinalaura@gmail.com>
2021-06-10 22:00:25 -0300

    add .ajax para habilidad comentado

--- src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/habilidad/controller/HabilidadController.java
+++ src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/habilidad/controller/HabilidadController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
@@ -15,13 +17,14 @@ public class HabilidadController {
     @Autowired
     IHabilidadService habilidadService;
 
+    @ResponseBody
     @GetMapping("/")
-    public String findAll(Model model){
-    //public List<Habilidad> findAll(Model model){
+    //public String findAll(Model model){
+    public List<Habilidad> findAll(Model model){
         List<Habilidad> habilidades = habilidadService.findAll();
         model.addAttribute("title","Lista de habilidades");
         model.addAttribute("habilidades", habilidades);
-        return "habilidad/findAll";
-        //return habilidades;
+        //return "habilidad/findAll";
+        return habilidades;
     } //capaz falte otra cosita aca
 }
