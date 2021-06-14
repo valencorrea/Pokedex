9f60cf1dd0f9e490843a4bbbaf44abd0cf446430
Valentina <correavalentinalaura@gmail.com>
2021-06-14 10:04:43 -0300

    add tabla para los evoluciones

--- src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/evolucion/controller/EvolucionController.java
+++ src/main/java/com/certant/pokedexRefactorValen/pokemon/atributosPokemon/evolucion/controller/EvolucionController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
@@ -15,11 +17,14 @@ public class EvolucionController {
     @Autowired
     IEvolucionService evolucionService;
 
+    @ResponseBody
     @GetMapping("/")
-    public String findAll(Model model){
+    //public String findAll(Model model){
+    public List<Evolucion> findAll(Model model){
         List<Evolucion> evoluciones = evolucionService.findAll();
         model.addAttribute("title","Lista de evoluciones");
         model.addAttribute("evoluciones", evoluciones);
-        return "evolucion/findAll";
+        //return "evolucion/findAll";
+        return  evoluciones;
     }
 }
